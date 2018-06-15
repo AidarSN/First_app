7c1db88d88a810b26f1922800c4b39f054b76139
Aidrt Sirazutdinov <aidar.sirazutdinovz@dnateam.ru>
2018-06-15 10:58:42 +0300

    Complete First_App

---
+++ app/models/ability.rb
@@ -0,0 +1,50 @@
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    Rails.logger.info '===================='
+
+    can :read, :all
+    user ||= User.new # guest user (not logged in)
+
+    Rails.logger.info user.inspect
+    if  user.role == 'admin'
+      can :manage, :all
+    elsif user.role == 'supervisor'
+      can :manage, [Customer, Order]
+      cannot :destroy, Customer
+      cannot :read, User
+    elsif user.role == 'manager'
+      can :manage, Order
+      cannot :read, User
+    end
+  end
+end
